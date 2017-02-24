3ea6b527dee88781df55c835f612c25c2bd6a4dc
vic-volk <vic-volk@yandex.ru>
2017-02-25 00:47:07 +0300

    Ontology: test;

--- communication-agent-module/src/test/java/ru/vlk/book/store/agent/test/AgentMemoryServiceTest.java
+++ communication-agent-module/src/test/java/ru/vlk/book/store/agent/test/AgentMemoryServiceTest.java
@@ -2,7 +2,10 @@ package ru.vlk.book.store.agent.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.AutoIRIMapper;
+import org.semanticweb.owlapi.util.SimpleIRIMapper;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,6 +13,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.vlk.book.store.agent.service.AgentMemoryService;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Component
@@ -21,8 +26,29 @@ public class AgentMemoryServiceTest {
     private AgentMemoryService agentMemoryService;
 
     @Test
-    public void testOWL() {
-        OWLOntology owlOntology = agentMemoryService.getOntology();
+    public void testOWL() throws OWLOntologyStorageException, OWLOntologyCreationException, IOException {
+        OWLDataFactory owlDataFactory = OWLManager.getOWLDataFactory();
+        OWLOntologyManager owlOntologyManager = OWLManager.createOWLOntologyManager();
 
+        IRI iri = IRI.create("http://www.semanticweb.org/");
+
+        ///
+        File output = File.createTempFile("sample_ontology.", "owl");
+        IRI documentIRI = IRI.create(output);
+        SimpleIRIMapper mapper = new SimpleIRIMapper(iri, documentIRI);
+        owlOntologyManager.getIRIMappers().add(mapper);
+        File localFolder = new File("materializedOntologies");
+        owlOntologyManager.getIRIMappers().add(new AutoIRIMapper(localFolder, true));
+        OWLOntology o = owlOntologyManager.createOntology(iri);
+
+        OWLClass clsA = owlDataFactory.getOWLClass(IRI.create(iri + "#A"));
+        OWLClass clsB = owlDataFactory.getOWLClass(IRI.create(iri + "#B"));
+
+        OWLAxiom axiom = owlDataFactory.getOWLSubClassOfAxiom(clsA, clsB);
+        AddAxiom addAxiom = new AddAxiom(o, axiom);
+        owlOntologyManager.applyChange(addAxiom);
+
+
+        owlOntologyManager.saveOntology(o);
     }
 }
